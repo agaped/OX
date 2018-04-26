da7cfcc90853ec4030d1714015c0db2a5136c6af
agaped <aga.pedlewska@gmail.com>
2018-04-26 23:33:14 +0200

    adds methods to validate user input and empty field in Board class, temporarily prints '+' as default character

--- src/main/java/com/gamelogic/Board.java
+++ src/main/java/com/gamelogic/Board.java
@@ -1,41 +1,57 @@
 package com.gamelogic;
 
+import java.util.Arrays;
 import java.util.function.Consumer;
 
 public class Board {
 
-    private char emptyChar='\u0000';
+    private char emptyChar = '\u0000';
+    private char plusChar = '\u002B';
     private GameConfig gameConfig;
     private char[][] boardState;
 
 
     public Board(GameConfig gameConfig) {
         this.gameConfig = gameConfig;
-        this.boardState=new char[gameConfig.getBoardLength()][gameConfig.getBoardWidth()];
-    }
+        this.boardState = new char[gameConfig.getBoardLength()][gameConfig.getBoardWidth()];
 
-    //todo: refactor to use Consumer<String> output or Message/Error Handling
-
-    public void addMove(Coordinates coordinates, Player currentPlayer){
-        int x=coordinates.getX()-1;
-        int y=coordinates.getY()-1;
-        if(getCheckIfMoveIsPossible(x, y)) {
-            this.boardState[x][y] = currentPlayer.name().charAt(0);
-        }else{
-            System.out.println("Move not possible");
+        for (char[] row : this.boardState) {
+            Arrays.fill(row, plusChar);
         }
     }
 
-    private boolean getCheckIfMoveIsPossible(int x, int y) {
-        if(this.boardState[x][y]==this.emptyChar){
+    public void addMove(Coordinates coordinates, Player currentPlayer, Consumer<String> output) {
+        int x = coordinates.getX() - 1;
+        int y = coordinates.getY() - 1;
+        if (checkIfUserInputIsCorrect(x, y)) {
+            if (checkIfMoveIsPossible(x, y)) {
+                this.boardState[x][y] = currentPlayer.name().charAt(0);
+            } else {
+                output.accept("Move not possible! Loosing turn....");
+            }
+        } else {
+            output.accept("Wrong input! Loosing turn....");
+        }
+    }
+
+    private boolean checkIfUserInputIsCorrect(int x, int y) {
+        if (x >= 0 && x < boardState.length && y >= 0 && y < boardState[0].length) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
-    public void printState(Consumer<String> output){
-        for (int i=0; i<this.boardState.length; i++){
+    private boolean checkIfMoveIsPossible(int x, int y) {
+        if (this.boardState[x][y] == this.plusChar) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void printState(Consumer<String> output) {
+        for (int i = 0; i < this.boardState.length; i++) {
             output.accept(String.valueOf(this.boardState[i]));
         }
 
