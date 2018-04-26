3d5f8e7400ebb16a8a1ca9bfc67ab61e78fdc243
agaped <aga.pedlewska@gmail.com>
2018-04-26 09:17:06 +0200

    refactors board class

--- src/main/java/com/gamelogic/Board.java
+++ src/main/java/com/gamelogic/Board.java
@@ -1,19 +1,15 @@
 package com.gamelogic;
 
+import java.util.function.Consumer;
+
 public class Board {
 
-    private int k;
 
-    public Board(int x, int y, int k){
-        int[][] board=new int[x][y];
-        this.k=k;
-    }
-
-    public void addMove(){
+    public void addMove(Coordinates coordinates, Player currentPlayer){
 
     }
 
-    public void print(){
-        System.out.println(this);
+    public void printState(Consumer<String> output){
+        output.accept("Board state");
     }
 }
