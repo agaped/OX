7be48214cfaaad18de2c95f98244676baf297be5
agaped <aga.pedlewska@gmail.com>
2018-04-26 09:15:04 +0200

    adds Coordinates

---
+++ src/main/java/com/gamelogic/Coordinates.java
@@ -0,0 +1,26 @@
+package com.gamelogic;
+
+public class Coordinates {
+    private  int x;
+    private  int y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public static Coordinates parse(String coordinatesAsString) {
+        String[] parts = coordinatesAsString.split(" ");
+        int x = Integer.parseInt(parts[0]);
+        int y = Integer.parseInt(parts[1]);
+        return new Coordinates(x, y);
+    }
+}
