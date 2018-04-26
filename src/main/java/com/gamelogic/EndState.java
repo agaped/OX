7c2de1e0173128c1734721e589e519e49f0033ea
agaped <aga.pedlewska@gmail.com>
2018-04-26 09:18:28 +0200

    adds End state and GameConfig class

---
+++ src/main/java/com/gamelogic/EndState.java
@@ -0,0 +1,18 @@
+package com.gamelogic;
+
+import java.util.function.Consumer;
+
+public class EndState implements GameState {
+    @Override
+    public void beginCurrentState(Consumer<String> output) {
+        output.accept("\nThanks for playing!\nStatistics:....\nDo you want to play again? y/n");
+    }
+
+    @Override
+    public GameState moveToTheNextState(String userInput) {
+//        if (userInput.equals("y"))
+//            return new InitialState(new GameConfig());
+//        else return null;
+        return new InitialState(new GameConfig());
+    }
+}
