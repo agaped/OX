d840ab1e93dddb49122c6da3e43e19482e43d217
Aga <aga.pedlewska@gmail.com>
2018-04-27 12:31:24 +0200

    adds validation of  given combination to win in GameConfig

--- src/main/java/com/gamelogic/GameConfig.java
+++ src/main/java/com/gamelogic/GameConfig.java
@@ -5,7 +5,7 @@ import java.util.function.Supplier;
 
 public class GameConfig {
 
-    private int maxNumberOfMatches=3;
+    private int maxNumberOfMatches = 3;
     private int numberCombinationToWin;
     private Coordinates boardCoordinates;
     private int boardLength;
@@ -15,27 +15,40 @@ public class GameConfig {
     public void setBoardSize(Consumer<String> output, Supplier<String> userInput) {
         String input;
         output.accept("Welcome to XO game!!!\nSome setup at the beggining...\n");
-        do{
+        do {
             output.accept("Provide board dimension y-vertical, x-horizontal: ");
-            input=userInput.get();
+            input = userInput.get();
 
-        }while(!input.matches("[1-9][0-9]*[\" \"][1-9][0-9]*"));
+        } while (!input.matches("[1-9][0-9]*[\" \"][1-9][0-9]*"));
 
-        boardCoordinates=Coordinates.parse(input);
-        this.boardWidth=boardCoordinates.getX();
-        this.boardLength=boardCoordinates.getY();
+        boardCoordinates = Coordinates.parse(input);
+        this.boardWidth = boardCoordinates.getX();
+        this.boardLength = boardCoordinates.getY();
     }
 
     //todo: validate user input
-    public void setNumberCombinationToWin(Consumer<String> output, Supplier<String> userInput){
-        output.accept("Provide length of combination to win: ");
-        this.numberCombinationToWin=Integer.parseInt(userInput.get());
+    public void setNumberCombinationToWin(Consumer<String> output, Supplier<String> userInput) {
+        while (true) {
+            output.accept("Provide length of combination to win: ");
+            String input = userInput.get();
+            int possibleCombination = 0;
+
+            if (input.matches("[1-9][0-9]*")) {
+                possibleCombination = Integer.parseInt(input);
+                if (possibleCombination <= this.boardLength || possibleCombination <= this.boardWidth) {
+                    this.numberCombinationToWin = possibleCombination;
+                    break;
+                }
+            }
+            output.accept("Impossible combination!!");
+        }
     }
 
     public int getBoardLength() {
         return this.boardLength;
     }
-    public int getBoardWidth(){
+
+    public int getBoardWidth() {
         return this.boardWidth;
     }
 }
